09af621be9b1f85cfcc3c2da94c3262bfcd7794e
W12woj34 <woj120@gmail.com>
2019-05-23 11:38:02 +0200

    add button to switch between fragments

--- app/src/main/java/com/example/flickrpublicfeed/DetailsActivity.kt
+++ app/src/main/java/com/example/flickrpublicfeed/DetailsActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.example.flickrpublicfeed.fragments.SimilarPicturesAdapter
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var viewPager : ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +18,12 @@ class DetailsActivity : AppCompatActivity() {
         val record = intent.getParcelableExtra(FPF_KEY_RECORD) as Record
         val similarPictures = intent.getStringArrayListExtra(FPF_KEY_SIMILAR_PHOTOS)
 
-        val viewPager = fragmentSwitcher
+        viewPager = this.fragmentSwitcher
         viewPager.adapter = SimilarPicturesAdapter(record, similarPictures, supportFragmentManager)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar, menu)
+        menuInflater.inflate(R.menu.menu, menu)
         return true
     }
 
@@ -29,8 +31,12 @@ class DetailsActivity : AppCompatActivity() {
 
         val id = item!!.itemId
 
-        if (id == R.id.home) {
-            onBackPressed()
+        if (id == R.id.add) {
+            if (viewPager.currentItem == 1) {
+                viewPager.currentItem = 0
+            } else {
+                viewPager.currentItem = 1
+            }
         }
 
         return super.onOptionsItemSelected(item)
